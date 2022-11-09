20ff32f0757d0b26b9536a863f4ac0fb199458f8
ryzen666 <htoohtetaung26@gmail.com>
2022-11-09 19:10:05 +0630

    b command

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ const _document = () => {
     <Html>
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
             <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet"/>
         </Head>
         <body>
