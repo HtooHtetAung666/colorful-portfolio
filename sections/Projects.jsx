e6faf2e6562f7c57d840d73541f258e646aa7e2c
ryzen666 <htoohtetaung26@gmail.com>
2023-01-10 15:25:16 +0630

    add padding to project item for sm screen size

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -7,7 +7,7 @@ export const Projects = () => {
   return (
     <div id='projects' className='w-full px-2 py-10 md:mt-64 md:mb-52 lg:mt-0 lg:mb-0 lg:min-h-screen'>
       <h1 className='text-center tracking-widest text-indigo-400 text-xl md:text-2xl lg:text-3xl'>Projects</h1>
-      <div className='grid md:grid-cols-2 gap-10 mt-10 p-2 lg:p-20'>
+      <div className='grid md:grid-cols-2 gap-10 mt-10 p-5 lg:p-20'>
 
         <ProjectItem title='chatRoom' backgroundImg={chatRoom} projectUrl='https://chat-room-bce12.web.app/' buildWith='Vue Js / Firebase'/>
 
