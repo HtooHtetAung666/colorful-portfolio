b6ead77aa20934dc33aee8aaf1c5d9108c9cf8db
ryzen666 <htoohtetaung26@gmail.com>
2023-02-06 11:23:40 +0630

    avoid hydration mismatch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,22 +12,14 @@ export const Navbar = () => {
         setMounted(true)
     },[]);
 
-    const handleNav = () => {
-        setNav(!nav);
+    if(!mounted) {
+        return null;
     }
 
-    const {systemTheme,theme,setTheme} = useTheme();
+    const {theme,setTheme} = useTheme();
 
-    const themeChanger = () => {
-        if(!mounted) return null;
-
-        const currentTheme = theme === 'system' ? systemTheme : theme;
-
-        if(currentTheme==='dark') {
-            return setTheme('light');
-        }else {
-            return setTheme('dark');
-        }
+    const handleNav = () => {
+        setNav(!nav);
     }
 
     return (
@@ -57,7 +49,7 @@ export const Navbar = () => {
                     </ul>
 
                     <label className="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" className="sr-only peer" onClick={()=>themeChanger()}/>
+                        <input type="checkbox" value="" className="sr-only peer" onClick={()=>setTheme(theme === 'dark' ? 'light' : 'dark')}/>
                         <div className="w-11 h-6 bg-[#ecf0f3] ring-2 ring-white dark:ring-black rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-800"></div>
                     </label>
 
