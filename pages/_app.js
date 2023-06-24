13488e55eb460a6e0279ef6690a68f2c204a3cd6
HtooHtetAung666 <htoohtetaung26@gmail.com>
2023-06-24 07:46:26 +0630

    write clean code in public folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={false} attribute="class">
+      <Head>
+        <title>Htoo Htet Aung</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
